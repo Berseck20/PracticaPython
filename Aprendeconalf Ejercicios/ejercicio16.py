fe17df1f31e5f308b56664a3de1e16c71d7a57aa
Luciano <lucianorusso97@gmail.com>
2022-05-17 12:42:38 -0300

    Ejercicio 16 Completado

---
+++ Aprendeconalf Ejercicios/ejercicio16.py
@@ -0,0 +1,13 @@
+"""
+Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34,
+y la extensión tiene dos dígitos (por ejemplo +34-913724710-56).
+Escribir un programa que pregunte por un número de teléfono con este formato y
+muestre por pantalla el número de teléfono sin el prefijo y la extensión.
+"""
+
+def run():
+    telefono = input("Ingrese su numero de telefono con el prefijo y la extension: ")
+    print(f"Su Numero De Telefono Es: {telefono[3:len(telefono)-2]}")
+
+if __name__ == "__main__":
+    run()

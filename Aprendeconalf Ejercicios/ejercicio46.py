f61e736a78b8a7e2fb3e63ad73f1d3e5fd956413
Luciano <lucianorusso97@gmail.com>
2022-06-24 13:04:15 -0300

    Ejercicio 46 Resuelto

---
+++ Aprendeconalf Ejercicios/ejercicio46.py
@@ -0,0 +1,15 @@
+"""
+Escribir un programa que almacene las asignaturas de un curso
+(por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+"""
+
+def run():
+    asignaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+    print("Tus Asignaturas Este Año Seran: ")
+    for n in asignaturas:
+        print(n)
+
+
+
+if __name__ == "__main__":
+    run()

455b563466a88388d5b40411750389498c1b9c4e
Luciano <lucianorusso97@gmail.com>
2022-07-25 13:14:21 -0300

    Ejercicio 68 resuelto

---
+++ Aprendeconalf Ejercicios/ejercicio68.py
@@ -0,0 +1,12 @@
+"""
+Escribir una función que muestre por pantalla el saludo ¡Hola amiga! cada vez que se la invoque.
+"""
+def saludo():
+    print("Hola Amiga!")
+
+def main():
+    saludo()
+
+
+if __name__ == "__main__":
+    main()

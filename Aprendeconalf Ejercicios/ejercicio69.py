d16c3477e58eff6f43fba42451ee977565d40c33
Luciano <lucianorusso97@gmail.com>
2022-07-26 11:17:54 -0300

    Ejercicio 69 completado

---
+++ Aprendeconalf Ejercicios/ejercicio69.py
@@ -0,0 +1,15 @@
+"""
+Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+"""
+
+def saludo():
+    nombre = "Gerardo"
+    print(f"Hola {nombre}")
+
+
+def main():
+    saludo()
+
+
+if __name__ == "__main__":
+    main()
